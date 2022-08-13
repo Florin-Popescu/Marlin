1b77b6c5415ed90ab2797c5490fea9c3049a56bf
Florin Popescu <florinsgpopescu@gmail.com>
2022-08-13 12:58:00 +0200

    higher pla preheat temperature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1992,8 +1992,8 @@
 // Preheat Constants - Up to 6 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 150
-#define PREHEAT_1_TEMP_BED     50
+#define PREHEAT_1_TEMP_HOTEND 185
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
