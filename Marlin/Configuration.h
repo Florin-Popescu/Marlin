ffd66ec158ba6d22eb10fef4366ce9634d6a2961
Florin Popescu <florinsgpopescu@gmail.com>
2022-08-23 19:56:07 +0200

    endstop position on Y should be 0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1494,7 +1494,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 5
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 16
 #define Y_MAX_POS Y_BED_SIZE + 5
