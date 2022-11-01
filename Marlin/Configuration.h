3208efc31a6ee8723e697b89718abf0e39ff60e0
Florin Popescu <florinsgpopescu@gmail.com>
2022-11-01 21:53:42 +0100

    pid tuned for new extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -654,9 +654,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  25.26
-    #define DEFAULT_Ki   2.22
-    #define DEFAULT_Kd  72.20
+    #define DEFAULT_Kp  14.15
+    #define DEFAULT_Ki   0.90
+    #define DEFAULT_Kd  55.42
   #endif
 #endif
 
